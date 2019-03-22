69c734c0ca2109a898c509f46094956bbe4d3f50
rijinmk <rijin.mk9@gmail.com>
2019-03-22 14:34:56 +0400

    Scanning light QR Code

--- RaspberryPi/Display/GenerateQRCode/main.js
+++ RaspberryPi/Display/GenerateQRCode/main.js
@@ -1,3 +1,11 @@
 var baseurl = 'https://speculo-home-automation.herokuapp.com/login';
-var qrcode = new QRCode('qrcode');
+
+var qrcode = new QRCode('qrcode', {
+	width: 128,
+	height: 128,
+	colorDark: '#fff',
+	colorLight: '#000',
+	correctLevel: QRCode.CorrectLevel.H
+});
+
 qrcode.makeCode(baseurl);
