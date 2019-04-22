0cdc0dabc0da4dd2d3570de887706c275acae177
rijinmk <rijin.mk9@gmail.com>
2019-04-23 00:13:29 +0400

    Garage Functionality

--- RaspberryPi/Graphs/graph.js
+++ RaspberryPi/Graphs/graph.js
@@ -1,6 +1,6 @@
-const xValue = d => new Date(d.time - 5 * 60 * 1000);
+const xValue = (d) => new Date(d.time - 5 * 60 * 1000);
 const xLabel = 'Time';
-const yValue = d => d.temprature;
+const yValue = (d) => d.temprature;
 const yLabel = 'Temperature';
 const margin = { left: 120, right: 30, top: 20, bottom: 120 };
 
@@ -10,70 +10,44 @@ const height = svg.attr('height');
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
-const g = svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
-const xAxisG = g.append('g')
-    .attr('transform', `translate(0, ${innerHeight})`);
+const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+const xAxisG = g.append('g').attr('transform', `translate(0, ${innerHeight})`);
 const yAxisG = g.append('g');
 
-xAxisG.append('text')
-    .attr('class', 'axis-label')
-    .attr('x', innerWidth / 2)
-    .attr('y', 100)
-    .text(xLabel);
+xAxisG.append('text').attr('class', 'axis-label').attr('x', innerWidth / 2).attr('y', 100).text(xLabel);
 
-yAxisG.append('text')
-    .attr('class', 'axis-label')
-    .attr('x', -innerHeight / 2)
-    .attr('y', -60)
-    .attr('transform', `rotate(-90)`)
-    .style('text-anchor', 'middle')
-    .text(yLabel);
+yAxisG
+	.append('text')
+	.attr('class', 'axis-label')
+	.attr('x', -innerHeight / 2)
+	.attr('y', -60)
+	.attr('transform', `rotate(-90)`)
+	.style('text-anchor', 'middle')
+	.text(yLabel);
 
 const xScale = d3.scaleTime();
 const yScale = d3.scaleLinear();
 
-const xAxis = d3.axisBottom()
-    .scale(xScale)
-    .tickPadding(15)
-    .ticks(5)
-    .tickSize(-innerHeight)
-
+const xAxis = d3.axisBottom().scale(xScale).tickPadding(15).ticks(5).tickSize(-innerHeight);
 
 const yTicks = 5;
-const yAxis = d3.axisLeft()
-    .scale(yScale)
-    .ticks(yTicks)
-    .tickPadding(15)
-    .tickSize(-innerWidth);
+const yAxis = d3.axisLeft().scale(yScale).ticks(yTicks).tickPadding(15).tickSize(-innerWidth);
 
-const line = d3.line()
-    .x(d => xScale(xValue(d)))
-    .y(d => yScale(yValue(d)))
-    .curve(d3.curveBasis);
+const line = d3.line().x((d) => xScale(xValue(d))).y((d) => yScale(yValue(d))).curve(d3.curveBasis);
 
-const row = d => {
-    d.timestamp = new Date(d.time - 5 * 60 * 1000);
-    d.temprature = +d.temprature;
-    return d;
+const row = (d) => {
+	d.timestamp = new Date(d.time - 5 * 60 * 1000);
+	d.temprature = +d.temprature;
+	return d;
 };
 
-d3.csv('http://localhost:8000/graph.csv', row, data => {
-    xScale
-        .domain(d3.extent(data, xValue))
-        .range([0, innerWidth]);
+d3.csv('graph.csv', row, (data) => {
+	xScale.domain(d3.extent(data, xValue)).range([ 0, innerWidth ]);
 
-    yScale
-        .domain(d3.extent(data, yValue))
-        .range([innerHeight, 0])
-        .nice(yTicks);
+	yScale.domain(d3.extent(data, yValue)).range([ innerHeight, 0 ]).nice(yTicks);
 
-    g.append('path')
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-width', 4)
-        .attr('d', line(data));
+	g.append('path').attr('fill', 'none').attr('stroke', 'steelblue').attr('stroke-width', 4).attr('d', line(data));
 
-    xAxisG.call(xAxis);
-    yAxisG.call(yAxis);
+	xAxisG.call(xAxis);
+	yAxisG.call(yAxis);
 });
---
+++ RaspberryPi/speculo-home-automation/assets/script/garage.js
@@ -0,0 +1,40 @@
+const IP_GARAGE_RASP = 'http://192.168.1.15';
+
+// TURN ON AND OFF THE APPLIANCES (CHECKBOX)
+$('.switch input[type=checkbox]').on('change', function() {
+	var stat = $(this).is(':checked') ? 1 : 0;
+	var link = $(this).attr('data-href');
+	$.get(IP_GARAGE_RASP + link + stat);
+	if (!stat) {
+		$('.icon svg').attr('class', 'fa fa-door-closed');
+	} else {
+		$('.icon svg').attr('class', 'fa fa-door-open');
+	}
+});
+
+$('#set_np').click(function() {
+	let n1 = $('#n1').val().replace(/\s/g, '');
+	let n2 = $('#n2').val().replace(/\s/g, '');
+	let n3 = $('#n3').val().replace(/\s/g, '');
+	let n123 = `${n1},${n2},${n3}`;
+	$.get(IP_GARAGE_RASP + '/allowed_np?q=' + n123);
+
+	$.get(`${IP_GARAGE_RASP}/get_nps`, function(data) {
+		let nps = data.split(',');
+		$('#garage ul').html('');
+		for (var i = 0; i < nps.length; i++) {
+			$('#garage ul').append(`<li>${nps[i]}</li>`);
+		}
+	});
+
+	$('.np_inp').val('');
+});
+
+$(document).ready(function() {
+	$.get(`${IP_GARAGE_RASP}/get_nps`, function(data) {
+		let nps = data.split(',');
+		for (var i = 0; i < nps.length; i++) {
+			$('#garage ul').append(`<li>${nps[i]}</li>`);
+		}
+	});
+});
