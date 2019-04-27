0f2285dee268bec050e64daf5911a178ef7528a7
rijinmk <rijin.mk9@gmail.com>
2019-04-27 14:52:40 +0400

    Garage Door State

--- RaspberryPi/Garage/garageDoorState.py
+++ RaspberryPi/Garage/garageDoorState.py
@@ -13,20 +13,19 @@ f = open('garage_info/garage_door_state.txt', 'r')
 state = f.read()
 f.close()
 
-print(state)
+print("------>", state)
 if(int(state)):
 	print(12.5)
-	p.ChangeDutyCycle(12.5)
+	p.start(12.5)
 else:
 	print(3)
-	p.ChangeDutyCycle(3)
+	p.start(3)
 
 while True: 
-	print(True)
 	f = open('garage_info/garage_door_state.txt', 'r')
 	state = f.read()
 	f.close()
-	
+	print(int(state))
 	if(int(state)):
 		p.ChangeDutyCycle(12.5)
 	else:
