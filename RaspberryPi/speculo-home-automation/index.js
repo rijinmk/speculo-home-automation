bebecf998ee703a68c867c8d8ba530884d067a34
rijinmk <rijin.mk9@gmail.com>
2019-06-08 20:16:48 +0400

    Chapter 3 - Details

--- RaspberryPi/speculo-home-automation/index.js
+++ RaspberryPi/speculo-home-automation/index.js
@@ -7,7 +7,7 @@ var User = require('./models/users');
 var fs = require('fs');
 
 const PORT = process.env.PORT || 3011;
-const IP_OF_NODEMCU = 'http://172.17.96.92';
+const IP_OF_NODEMCU = 'http://172.17.101.196';
 var app = express();
 
 // Firebase for WAN
